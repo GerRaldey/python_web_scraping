635956a0b027a8f6c4a939e054df5be97851a750
Gerald Miñoza <38746358+GerRaldey@users.noreply.github.com>
2020-06-11 15:09:20 +0800

    Add files via upload

---
+++ locators/all_books_pages.py
@@ -0,0 +1,3 @@
+class AllBooksPageLocator:
+    Books = 'div.col-sm-8 section ol.row li.col-xs-6'
+    PAGER = 'section ul.pager li.current'
---
+++ locators/book_locators.py
@@ -0,0 +1,5 @@
+class BookLocators:
+    Title_Locator = 'article.product_pod h3 a'
+    Link_Locator = 'article.product_pod h3 a'
+    Price_Locator = 'article.product_pod div.product_price p.price_color'
+    Rating_Locator = 'article.product_pod p.star-rating'
